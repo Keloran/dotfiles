68fd4807aec11d6f35512900094007512142e7af
Keloran <Keloran@develbox.info>
2015-11-16 13:21:12 +0000

    lots of changes

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+git pull origin master
+function doIt() {
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av --no-perms . ~
+    source ~/.zshrc
+}
+
+if [ "$1" == "--force" -o "$1" == "-f"]; then
+    doIt
+else
+    read -p "This is going to override stuffs" -n 1
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt
+    fi
+fi
+unset doIt
