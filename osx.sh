f71c91e59ec43e9ec27d178e3f9f802f96997d06
Keloran <keloran@nordicarts.net>
2015-11-17 16:07:34 +0000

    changed some quick stuffs

--- osx.sh
+++ osx.sh
@@ -1,13 +1,63 @@
 # Install Brew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+./brew.sh
 
 # Oh my ZSH
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Statusbar in finder
+# Screen
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 5
+
+# Finder
 defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.finder ShowPathBar -bool true
 
-# Debug menu in Safari
+killall Finder > /dev/null 2>&1
+
+# Safari
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari HomePage -string "about:blank"
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+defaults write com.apple.Safari WebKitDeveloperExtras -bool true
 
+killall Safari > /dev/null 2>&1
 
+# Mission Control
+defaults write com.apple.dashboard mxc-disabled -bool true
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# Spotlight
+defaults write com.apple.spotlight orderedItems -array \
+	'{"enabled" = 1;"name" = "APPLICATIONS";}' \
+	'{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
+	'{"enabled" = 1;"name" = "DIRECTORIES";}' \
+	'{"enabled" = 1;"name" = "CONTACT";}' \
+	'{"enabled" = 1;"name" = "DOCUMENTS";}' \
+	'{"enabled" = 1;"name" = "PDF";}' \
+	'{"enabled" = 0;"name" = "FONTS";}' \
+	'{"enabled" = 1;"name" = "MESSAGES";}' \
+	'{"enabled" = 0;"name" = "EVENT_TODO";}' \
+	'{"enabled" = 0;"name" = "IMAGES";}' \
+	'{"enabled" = 0;"name" = "BOOKMARKS";}' \
+	'{"enabled" = 0;"name" = "MUSIC";}' \
+	'{"enabled" = 0;"name" = "MOVIES";}' \
+	'{"enabled" = 0;"name" = "PRESENTATIONS";}' \
+	'{"enabled" = 0;"name" = "SOURCE";}'
+
+killall mds > /dev/null 2>&1
+sudo mdutil -i on / > /dev/null
+sudo mdutil -E / > /dev/null
+
+# Dock
+defaults write com.apple.dock show-process-indicators -bool true
+
+dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Safari.app"
+dockutil --no-restart --add "/Applications/System Preferences.app"
+dockutil --no-restart --add "$HOME/Applications/iTerm.app"
+
+killall Dock /dev/null 2>&1
