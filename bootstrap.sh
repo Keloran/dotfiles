415b5fe326b0137c9672f29faac542195fcf7dc6
Keloran <keloran@me.com>
2015-11-17 23:21:31 +0000

    bootstrap changed slightly

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-git pull origin master
+
+# Install xcode 
+sudo softwareupdate -i -a
+xcode-select --install
+
+# create the directory
+mkdir .dotfiles
+
+# clone into directory
+git clone git@github.com:Keloran/dotfiles.git .dotfiles/keloran
+
+# got into directory
+cd .dotfiles/keloran
+
 function doIt() {
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av --no-perms . ~
 	.~/installs/osx.sh
