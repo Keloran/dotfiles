448be5118a0d6fc5bc4b9a5f1994b3edf0a5dd0b
Keloran <keloran@nordicarts.net>
2015-11-17 16:48:01 +0000

    slight change

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,13 @@
 git pull origin master
 function doIt() {
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av --no-perms . ~
-	./installs/osx.sh
-	./installs/brew.sh
-	./installs/z.sh
+	.~/installs/osx.sh
+	.~/installs/brew.sh
+	.~/installs/z.sh
     source ~/.zshrc
 
 	# Remove the old files
-	rm -rf installs
+	rm -rf ~/installs
 }
 
 if [ "$1" == "--force" -o "$1" == "-f"]; then
