5ba43403529c87c73055bfb0f34b985895ba163e
Max Hooton <max.hooton@auden.co.uk>
2017-12-13 10:31:28 +0000

    updated the dock settings

--- installs/osx/dock.sh
+++ installs/osx/dock.sh
@@ -7,32 +7,38 @@ defaults write com.apple.dock wvous-br-corner -int 5
 defaults write com.apple.dock wvous-br-modifier -int 0
 
 dockutil --no-restart --remove all
+
 dockutil --no-restart --add "/Applications/Safari.app"
 
-if [ -d "/Applications/Dash.app" ]; then
+if [[ -d "/Applications/Min.app" ]]; then
+    dockutil --no-restart -add "/Applications/Min.app"
+fi
+
+
+if [[ -d "/Applications/Dash.app" ]]; then
 	dockutil --no-restart --add "/Applications/Dash.app"
 fi
 
-if [ -d "/Applications/Airmail 2.app" ]; then
-	dockutil --no-restart --add "/Appications/Airmail 2.app"
+if [[ -d "/Applications/Airmail 3.app" ]]; then
+	dockutil --no-restart --add "/Appications/Airmail 3.app"
 fi
 
 dockutil --no-restart --add "/Applications/System Preferences.app"
 
-if [ -d "$HOME/Applications/iTerm.app" ]; then
-	dockutil --no-restart --add "$HOME/Applications/iTerm.app"
+if [[ -d "/Applications/Hyper.app" ]]; then
+	dockutil --no-restart --add "/Applications/Hyper.app"
 fi
 
-if [ -d "$HOME/Applications/Slack.app" ]; then
-	dockutil --no-restart --add "$HOME/Applications/Slack.app"
+if [[ -d "/Applications/Slack.app" ]]; then
+	dockutil --no-restart --add "/Applications/Slack.app"
 fi
 
-if [ -d "$HOME/Applications/Sequel Pro.app" ]; then
-	dockutil --no-restart --add "$HOME/Applications/Sequel Pro.app"
+if [[ -d "/Applications/Sequel Pro.app" ]]; then
+	dockutil --no-restart --add "/Applications/Sequel Pro.app"
 fi
 
-if [ -d "$HOME/Applications/IntelliJ IDEA.app" ]; then
-	dockutil --no-restart --add "$HOME/Applications/IntelliJ IDEA.app"
+if [[ -d "/Applications/IntelliJ IDEA.app" ]]; then
+	dockutil --no-restart --add "/Applications/IntelliJ IDEA.app"
 fi
 
 killall Dock /dev/null 2>&1
