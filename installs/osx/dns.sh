cd99430bd4791cdde43051e866343a85a319131d
Max Hooton <max.hooton@auden.co.uk>
2019-01-16 12:01:23 +0000

    updated dnsmasq config

--- installs/osx/dns.sh
+++ installs/osx/dns.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 sudo curl -o /Library/LaunchDaemons/com.ralphschindler.docker_10254_alias.plist https://gist.githubusercontent.com/ralphschindler/535dc5916ccbd06f53c1b0ee5a868c93/raw/com.ralphschindler.docker_10254_alias.plist
 
+echo "alias=10.254.254.254,127.0.0.1" >> $(brew --prefix)/etc/dnsmasq.conf
 echo "address=/.devel/10.254.254.254" >> $(brew --prefix)/etc/dnsmasq.conf
+echo "address=/.local/10.254.254.254" >> $(brew --prefix)/etc/dnsmasq.conf
 echo "listen-address=127.0.0.1" >> $(brew --prefix)/etc/dnsmasq.conf
 
 brew services start dnsmasq
